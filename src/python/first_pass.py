b65577b7bc83dbd9f228a4139a839f11f86d565d
bspkrs <bspkrs@gmail.com>
2022-05-26 16:29:34 -0400

    last pass

--- src/python/first_pass.py
+++ src/python/first_pass.py
@@ -1,6 +1,5 @@
 __author__ = 'bspkrs'
 
-import datetime
 import time
 
 from flask import Flask, request, jsonify
@@ -8,7 +7,10 @@ from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
+# TODO: this is clearly something that we'd want to keep persisted between restarts of the application if it isn't empty
 errors = []
+temperature_threshold = 90.0
+DATA_PARSE_ERROR_MESSAGE = 'Data is not in required format'
 
 
 def _find_next_id():
@@ -17,19 +19,22 @@ def _find_next_id():
 
 @app.get("/errors")
 def get_errors():
-    return jsonify(errors), 200
+    return jsonify(dict(errors=errors)), 200
 
 
 @app.delete("/errors")
 def delete_errors():
-    global errors
-    errors = []
-    return jsonify(errors), 200
+    num_deleted = len(errors)
+    errors.clear()
+    return jsonify(dict(error_records_deleted=num_deleted)), 200
 
 
-def record_error(data):
-    errors.append(dict(id=_find_next_id(), data=data,
-                       received_ts=(datetime.datetime.now() + datetime.timedelta(seconds=time.timezone)).strftime("%Y/%m/%d %H:%M:%S")))
+def record_error(_request, msg=None):
+    data = _request.get_data(as_text=True)
+    # errors.append(dict(error_id=_find_next_id(), errant_data=data, message=msg,
+    #                    received_ts=(datetime.datetime.now() +
+    #                                 datetime.timedelta(seconds=time.timezone)).strftime("%Y/%m/%d %H:%M:%S")))
+    errors.append(data)
 
 
 @app.post("/temp")
@@ -39,55 +44,55 @@ def accept_temperature():
 
     # Attempt to retrieve the json
     try:
-        temperature = request.get_json()
+        temperature_data = request.get_json()
     except BadRequest:
-        record_error(request.get_data(as_text=True))
+        record_error(request, 'Invalid JSON')
         return jsonify(dict(error='bad request')), 400
 
-    # Get Device ID, and make sure its a valid number
-    device_id_str, _, rest = temperature["data"].partition(":")
+    # Get Device ID, and make sure it's a valid number
+    device_id_str, _, rest = temperature_data["data"].partition(":")
     if not _ or not rest:
-        record_error(request.get_data(as_text=True))
+        record_error(request, DATA_PARSE_ERROR_MESSAGE)
         return jsonify(dict(error='bad request')), 400
 
     try:
         device_id = int(device_id_str)
     except ValueError:
-        record_error(request.get_data(as_text=True))
+        record_error(request, 'Unable to parse device_id as integer')
         return jsonify(dict(error='bad request')), 400
 
-    # Get Epoch MS value, make sure its a valid number, and parse it into a formatted timestamp string
+    # Get Epoch MS value, make sure it's a valid number, and parse it into a formatted timestamp string
     epoch_ms_str, _, rest = rest.partition(":")
     if not _ or not rest:
-        record_error(request.get_data(as_text=True))
+        record_error(request, DATA_PARSE_ERROR_MESSAGE)
         return jsonify(dict(error='bad request')), 400
 
     try:
         epoch_ms = int(epoch_ms_str)
     except ValueError:
-        record_error(request.get_data(as_text=True))
+        record_error(request, 'Unable to parse Epoch MS as integer')
         return jsonify(dict(error='bad request')), 400
 
     s, ms = divmod(epoch_ms, 1000)
     try:
         formatted_ts = '{}.{:03d}'.format(time.strftime('%Y/%m/%d %H:%M:%S', time.gmtime(s)), ms)
     except ValueError:
-        record_error(request.get_data(as_text=True))
+        record_error(request, 'Unable to convert Epock MS to datetime')
         return jsonify(dict(error='bad request')), 400
 
     # Get 'Temperature' label and temperature value, test them, and return the appropriate result
     temp_label_str, _, temperature_str = rest.partition(":")
     if not _ or not temperature_str or temp_label_str != "'Temperature'":
-        record_error(request.get_data(as_text=True))
+        record_error(request, DATA_PARSE_ERROR_MESSAGE)
         return jsonify(dict(error='bad request')), 400
 
     try:
-        temperature = float(temperature_str)
+        temperature_data = float(temperature_str)
     except ValueError:
-        record_error(request.get_data(as_text=True))
+        record_error(request, 'Unable to parse temperature as float')
         return jsonify(dict(error='bad request')), 400
 
-    ret = dict(overtemp=(temperature >= 90.0))
+    ret = dict(overtemp=(temperature_data >= temperature_threshold))
     if ret["overtemp"]:
         ret.update(dict(device_id=device_id, formatted_time=formatted_ts))
 
